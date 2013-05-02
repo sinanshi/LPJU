387537205c35651ea7b2ea06074de550ddefc75c
SinanShi <shisinancn@gmail.com>
2013-05-02 12:26:48 +0200

    new window function

--- map.r
+++ map.r
@@ -51,8 +51,9 @@ map.show<-function(map.data,y,m){
  yg.palette <- colorRampPalette(c("yellow3","yellow","greenyellow","green","green3","darkgreen"))
  colo=yg.palette(101)
  colo.cm = cm.colors(32)
- image(x=seq(-19.75,49.75,len=140),y=seq(25.25,49.75,len=50),map.data[,,m,1],col=colo,xlab="",ylab="",axes=T)
+ image(x=seq(-19.75,49.75,len=140),y=seq(25.25,49.75,len=50),map.data[,,m,y],col=colo,xlab="",ylab="",axes=T)
   map(add=T,boundary=T)
+ title(main=paste(as.character(switch.year+simstartyear-1),"-",as.character(switch.month)))
 }
 
 
@@ -61,30 +62,11 @@ map.interact<-function(map.data,startyear,endyear,type,colour){
  
  startyear<-startyear-simstartyear+1
  endyear<-endyear-simstartyear+1
-#####################################
-#####################################
- yhscale <- 1.5    # Horizontal scaling
- Myvscale <- 1.5    # Vertical scaling
 
- tt <- tktoplevel()
- tkwm.title(tt,date)
-
- CopyToClip <- function(){
-   png(file="myplot.png", bg="white")
-   #display a map
-   dev.off()
-}
-
- copy.but <- tkbutton(tt,text="Copy to Clipboard",command=CopyToClip)
-
- tkgrid(copy.but)
- 
-
+#######################################
+#######################################
  switch.year<<-startyear
  switch.month<<-1
- ############################
- #
- #########################
  map.switch<-function(xlim=NULL, ylim=NULL, xaxs="r", yaxs="r"){
    
    
@@ -94,37 +76,80 @@ map.interact<-function(map.data,startyear,endyear,type,colour){
       if (dev.cur() != eventEnv$which) dev.set(eventEnv$which)
 
 
-   keydown <- function(key) {
+      keydown <- function(key) {
              if (key == "q") return(invisible(1))
-             eventEnv$onMouseMove <- NULL
+                 eventEnv$onMouseMove <- NULL
              if(key=="6"){
-               if(switch.month==12){
-                  switch.year<<-switch.year+1
-                  switch.month<<-1
+                if(switch.year==endyear&&switch.month==12){
+                   switch.year<<-startyear
+                   switch.month<<-1
+                }
+                else{
+                   if(switch.month==12){
+                      switch.year<<-switch.year+1
+                      switch.month<<-1
+                   }
+                   else
+                      switch.month<<-switch.month+1
+                }
+                map.show(map.data,switch.year,switch.month)
+                Sys.sleep(0.05) 
+             } 
+             if(key=="4"){
+               if(switch.year==startyear&&switch.month==1){
+                  switch.year<<-endyear
+                  switch.month<<-12
                }
                else{
-               switch.month<<-switch.month+1
-               }
-               map.show(map.data,switch.year,switch.month)
-               Sys.sleep(0.1) 
-               print(switch.month)
-              } 
-            if(key=="4"){
-              plot(rnorm(1000))
-              Sys.sleep(0.1)
-            }
+                  if(switch.month==1){
+                     switch.year<<-switch.year-1
+                     switch.month<<-12
+                  }
+                  else
+                   switch.month<<-switch.month-1 
+              }
+              map.show(map.data,switch.year,switch.month)
+              Sys.sleep(0.05)
+             
             NULL 
         }
-
-    setGraphicsEventHandlers(prompt="Click and drag, hit q to quit",
-                          onKeybd = keydown)
+     }
+    setGraphicsEventHandlers(prompt="hit q to quit",
+                             onKeybd = keydown)
     eventEnv <- getGraphicsEventEnv()
 
 
 }
 
+#####################################
+#####################################
+ yhscale <- 1.5    # Horizontal scaling
+ Myvscale <- 1.5    # Vertical scaling
 
-     X11(type = "Xlib")
+ CopyToClip <- function(){
+   png(file=paste(as.character(switch.year+simstartyear-1),"-",as.character(switch.month)), bg="white")
+   map.show(map.data,switch.year,switch.month)
+   dev.off()
+}
+
+ NewWin<- function(){
+   X11(type = "Xlib")#X11, remove this line if the system is not X11
+   map.show(map.data,switch.year,switch.month)
+ }
+
+
+#####################################
+#####################################
+     tt <- tktoplevel()
+     tkwm.title(tt,date)
+     copy.but <- tkbutton(tt,text="Copy to Clipboard",command=CopyToClip)
+     newwin.but <- tkbutton(tt,text="Show in a new window",command=NewWin)
+     tkgrid(copy.but)
+     tkgrid(newwin.but)
+
+
+
+     X11(type = "Xlib")#X11, remove this line if the system is not X11
      map.switch(startyear,endyear)
      # This currently only works on the Windows
      # and X11(type = "Xlib") screen devices...
