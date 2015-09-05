a2108c73eb228e4dd9782c3dfb879af58bb08aea
sinanshi <shisinancn@gmail.com>
2015-09-05 18:16:36 +0100

    output yearband

--- read.output.r
+++ read.output.r
@@ -233,6 +233,25 @@ read.output.harvest<-function(filename, ncells, nbands, startyear, year,data.siz
 	close(fn)
 	return(harvest)
 }
+
+
+#read output 
+read.output.yearband <- function(filename, year, band, start_year, 
+                                 ncells, nyears, nbands, data.size = 4)
+{
+    ind_year <- year - start_year
+    file_out <- file(sprintf(filename),"rb")
+    pos_start <- data.size * (ind_year * nbands * ncells + (band - 1) * ncells)
+    cat(start_year,year,"\n")
+    #print(pos_start/data.size/ncells)
+    seek(file_out, where = pos_start, origin = "start")
+    data<-readBin(file_out, numeric(), n = ncells, size = data.size)
+
+    close(file_out)
+    return(data)
+}
+
+
     
 read.output.all<-function(path){
   pixel_year<-get.output.info(path)
