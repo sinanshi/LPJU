9e8999313ff6730a9a7d4a8d37e7e4fbc0feb746
sinanshi <shisinancn@gmail.com>
2015-09-13 04:29:03 +0100

    fix the bug the cout_ncdf_band of cft iterating in the time loop; add process bar; remove redandent function

--- ncdf/lpj2ncdf.r
+++ ncdf/lpj2ncdf.r
@@ -19,7 +19,7 @@ lpjoutput2ncdf <- function(lpjoutput)
     read.input.grid(lpjoutput[["lpjgrid"]])
     end_year <- lpjoutput[["start_year"]] + lpjoutput[["nyears"]] - 1
     count_ncdf_band <- 1
-
+    pb <- txtProgressBar(min = 0, max = lpjoutput[["nyears"]],style = 3)
     for(this_year in lpjoutput[["start_year"]]:end_year){
         for(this_band in 1:lpjoutput[["nbands"]]){
             this_data <- read.output.yearband(lpjoutput[["filename"]], 
@@ -33,46 +33,11 @@ lpjoutput2ncdf <- function(lpjoutput)
                 put.lpjvar.ncdf(nc_single_var, lpjoutput[["var_name"]], this_map, start = count_ncdf_band, count = 1)
                 count_ncdf_band <- count_ncdf_band + 1
             }
-            if(IS_MULTI_VAR)
-                count_ncdf_band <- count_ncdf_band + 1
-            cat("\b\b\b\b\b\b\b\b\b\b\b\b\b",((this_year + 1 -lpjoutput[["start_year"]])/lpjoutput[["nyears"]])*100,"%",sep="")
         }
+        if(IS_MULTI_VAR)
+            count_ncdf_band <- count_ncdf_band + 1
+        setTxtProgressBar(pb,(this_year + 1 -lpjoutput[["start_year"]]))
     }
+    close(pb)
     close.ncdf(nc_single_var[["nc"]])
 }
-
-#not usefule
-cft_lpjoutput2ncdf <- function(lpjoutput)
-{
-
-    nc_single_var <- 
-        new.var.ncdf(lpjoutput[["ncfile"]],
-                     lpjoutput[["lpjgrid"]],
-                     lpjoutput[["var_name"]],
-                     lpjoutput[["units"]],
-                     lpjoutput[["time_start"]],
-                     lpjoutput[["time_interval"]],
-                     lpjoutput[["time_dim"]],
-                     lpjoutput[["longname"]],
-                     lpjoutput[["missval"]])
-    read.input.grid(lpjoutput[["lpjgrid"]])
-    end_year <- lpjoutput[["start_year"]] + lpjoutput[["nyears"]] - 1
-    count_ncdf_band <- 1
-    for(this_year in lpjoutput[["start_year"]]:end_year){
-        for(this_band in 1:lpjoutput[["nbands"]]){
-
-            this_data <- read.output.yearband(lpjoutput[["filename"]], 
-                                              this_year, this_band, 
-                                              lpjoutput[["start_year"]], lpjoutput[["ncells"]], 
-                                              lpjoutput[["nyears"]], lpjoutput[["nbands"]])
-            this_map <- map.build(this_data)
-            put.lpjvar.ncdf(nc_single_var, lpjoutput[["var_name"]][this_band], this_map, start = count_ncdf_band, count = 1)
-        }
-        count_ncdf_band <- count_ncdf_band + 1
-
-    }
-    close.ncdf(nc_single_var[["nc"]])
-}
-
-
-
